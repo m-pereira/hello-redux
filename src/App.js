f08d9b7002560ab8fa217771b9d2ac17f518afc5
Mauricio Lima <mauricio@codebit.com.br>
2021-04-14 19:04:14 -0300

    feat: add global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
