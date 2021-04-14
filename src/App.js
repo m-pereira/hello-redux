5665e9619bd1da3b97cc426b01df2835ab2f48ca
Mauricio Lima <mauricio@codebit.com.br>
2021-04-14 19:31:21 -0300

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from './styles/global';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Cart } from './styles';
+import logo from '../../assets/images/logo.svg';
+import { MdShoppingBasket } from 'react-icons/md';
+
+function Header() {
+  return (
+    <Container>
+      <Link to='/'>
+        <img src={logo} alt='Rocketshoes' />
+      </Link>
+
+      <Cart to='/cart'>
+        <div>
+          <strong>Meu Carringo</strong>
+          <span>3 itens</span>
+          <MdShoppingBasket size={36} color='#fff' />
+        </div>
+      </Cart>
+    </Container>
+  );
+}
+
+export default Header;
